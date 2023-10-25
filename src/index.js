e5dff7336bee6bc6ccb35e748d53778406d2d034
yara mohamed <75863913+yara857@users.noreply.github.com>
2023-10-25 19:15:48 +0300

    backend is finished

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const path = require('path')
 const hbs = require('hbs')
-
+const collection = require('./mongodb')
 const templatePath=path.join(__dirname,'../templates')
 
 app.use(express.json())
@@ -11,12 +11,41 @@ app.set('view engine','hbs')
 app.set("views" , templatePath)
 // app.use('/public', express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({extended:false}))
+
 app.get('/',(req,res)=>{
     res.render('login')
 })
 
 app.get('/signup',(req,res)=>{
-    res.render('login')
+    res.render('signup')
+})
+
+app.post("/signup",async(req,res)=>{
+    const data = {
+        name:req.body.name,
+        password:req.body.password
+    }
+    await collection.insertMany([data])
+    console.log(data);
+    res.render('home')
+})
+
+
+app.post("/login",async(req,res)=>{
+   try{
+    const check = await collection.findOne({name:req.body.name})
+    if(check.password === req.body.password){
+        res.render("home")
+    }
+    else{
+        res.send("wrong password")
+    }
+
+   }catch{
+        res.send('worng details')
+   }
+   
 })
 
 app.listen(3000 , ()=>{
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,2 +1,26 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/')
+mongoose.connect('mongodb+srv://yara:123@cluster0.an7064r.mongodb.net/?retryWrites=true&w=majority')
+
+// mongoose.connect('mongodb://localhost:27017/LoginSignUpTutorial')
+    .then(() => {
+        console.log('mongodb connected')
+    })
+    .catch(() => {
+        console.log('failed to connect')
+    })
+
+const logInSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+
+
+const collection = new mongoose.model("Collection1", logInSchema)
+
+module.exports = collection
